60439c83eb4044f03005272df4c2946ec36503d7
davidcoats <david@getsafetycone.com>
2023-05-24 12:10:11 -0700

    Initial commit.

---
+++ source/R5T.T0203.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.T0203.Construction
+Construction console project for the R5T.T0203 library.
---
+++ source/R5T.T0203/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0203
+{
+	/// <summary>
+	/// XML text strong-type definition.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
